b09a73ea7bbf6cec1b41ca29586583cdf0101876
Карус <@DESKTOP-1NJUMFV>
2018-05-03 20:36:14 +0700

    init

--- JSK.Domain/Entities/TestQuestion.cs
+++ JSK.Domain/Entities/TestQuestion.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JSK.Domain.Entities
 {
-    [Table("TestQuestion")]
+    [Table("TestQuestions")]
     public class TestQuestion
     {
         [Key]
