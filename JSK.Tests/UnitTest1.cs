b4b5c189764ae2a7372e62ea34124ab42c9bbf16
Khusainov Ruslan <khusainovheckfy@gmail.com>
2018-05-08 10:16:06 +0700

    complete

--- JSK.Tests/UnitTest1.cs
+++ JSK.Tests/UnitTest1.cs
@@ -12,8 +12,34 @@ namespace JSK.Tests
         [Fact]
         public void TestCreateTests()
         {
-            
+            TestDTO test = new TestDTO()
+            {
+                IsActive = true,
+                IsRandomQuestionsOrder = true,
+                Name = "Test of test",
+            };
 
+            for (int i = 0; i < 5; i++)
+            {
+                TestQuestionDTO question = new TestQuestionDTO()
+                {
+                    Question = $"Question {i}",
+                    IsActive = true,
+                    IsSingleAnswer = false,
+                    TestQuestionAnswers = new System.Collections.Generic.List<TestQuestionAnswerDTO>()
+                    {
+                        new TestQuestionAnswerDTO(){Answer=$"Answer {i}_1", IsActive=true, IsCorrect=false },
+                        new TestQuestionAnswerDTO(){Answer=$"Answer {i}_2", IsActive=true, IsCorrect=false },
+                        new TestQuestionAnswerDTO(){Answer=$"Answer {i}_3", IsActive=true, IsCorrect=true },
+                        new TestQuestionAnswerDTO(){Answer=$"Answer {i}_4", IsActive=true, IsCorrect=false },
+                        new TestQuestionAnswerDTO(){Answer=$"Answer {i}_5", IsActive=true, IsCorrect=true },
+                        new TestQuestionAnswerDTO(){Answer=$"Answer {i}_6", IsActive=true, IsCorrect=true },
+                        new TestQuestionAnswerDTO(){Answer=$"Answer {i}_7", IsActive=true, IsCorrect=false }
+                    }
+                };
+            }
+            Mock<ITestService> mockService = new Mock<ITestService>();
+            mockService.Setup(arg => arg.Test_SaveAsync(test)).Returns(Task.FromResult(0));
 
         }
     }
